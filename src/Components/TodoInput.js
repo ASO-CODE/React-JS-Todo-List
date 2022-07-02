62b189a58d18ad6a7e656c484a4741b0d212d251
ASO-CODE <60587094+ASO-CODE@users.noreply.github.com>
2022-07-02 18:37:45 +0100

    Input Component updated

--- src/Components/TodoInput.js
+++ src/Components/TodoInput.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import tasklist from "../../src/tasklist.svg";
 export default class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
-      <div>TodoInput</div>
-    )
+      <div className="card card-body my-3">
+        <form action="" onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary tex-white">
+                <img
+                  src={tasklist}
+                  style={{ width: "20px", height: "30px" }}
+                  alt="tasklist img"
+                />
+              </div>
+            </div>
+            <input
+              type="text"
+              name=""
+              id=""
+              className="form-control text-capitalize"
+              placeholder="add todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button style={{width: '100%'}}
+            type="submit"
+            className="btn btn-block btn-primary mt-3 text-uppercase"
+          >
+            add item
+          </button>
+        </form>
+      </div>
+    );
   }
 }
